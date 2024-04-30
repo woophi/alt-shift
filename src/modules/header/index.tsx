2c4704a9aa17f84dbdbd0887c606f4cc0ac08169
Konstantin Mikheev <mik.kon6610@gmail.com>
2024-04-30 10:11:37 +0200

    added route links

--- src/modules/header/index.tsx
+++ src/modules/header/index.tsx
@@ -1,5 +1,7 @@
+import { routes } from '@/core/routes';
 import { IconButton } from '@/ui/icon-button';
 import Image from 'next/image';
+import Link from 'next/link';
 import { Ponits } from '../points';
 import { headerStyles } from './style.css';
 
@@ -9,7 +11,9 @@ export const Header = () => {
       <Image src="/imgs/logo.svg" width={179} height={44} alt="Alt+Shift logo" />
       <div className={headerStyles.rightItems}>
         <Ponits variant="bullets" />
-        <IconButton icon="home" />
+        <Link href={routes.home}>
+          <IconButton icon="home" />
+        </Link>
       </div>
     </header>
   );
--- src/modules/home/HomeLayout.tsx
+++ src/modules/home/HomeLayout.tsx
@@ -1,5 +1,7 @@
+import { routes } from '@/core/routes';
 import { Button } from '@/ui/button';
 import { typography } from '@/ui/styles/typography.css';
+import Link from 'next/link';
 import { Ponits } from '../points';
 import { homeStyles } from './style.css';
 
@@ -9,7 +11,9 @@ export const HomeLayout = () => {
       <div className={homeStyles.headContainer}>
         <h1 className={typography({ variant: 'h1', weight: 600 })}>Applications</h1>
 
-        <Button icon="plus">Create New</Button>
+        <Link href={routes.generator}>
+          <Button icon="plus">Create New</Button>
+        </Link>
       </div>
 
       <div className={homeStyles.contentContainer}>
@@ -18,9 +22,11 @@ export const HomeLayout = () => {
           <p className={typography({ variant: 'caption', font: 'text' })}>
             Generate and send out couple more job applications today to get hired faster
           </p>
-          <Button size="l" icon="plus">
-            Create New
-          </Button>
+          <Link href={routes.generator}>
+            <Button size="l" icon="plus">
+              Create New
+            </Button>
+          </Link>
           <Ponits variant="flattened" />
         </div>
       </div>
